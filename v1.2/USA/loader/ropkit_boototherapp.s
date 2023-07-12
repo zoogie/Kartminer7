db82f22eeb0c731cd263df1fb2e9dafed470f514
zoogie <stackrestacker@gmail.com>
2023-07-12 01:26:50 -0500

    More EUR tidbits

--- v1.2/USA/loader/ropkit_boototherapp.s
+++ v1.2/USA/loader/ropkit_boototherapp.s
@@ -40,32 +40,28 @@ ROP_SETR1 ROPKIT_TMPDATA+0x80
 
 @ Copy red to address and then gpu_writehwregs. we need to stay position independant
 CALLFUNC_NOSP GSP_WRITEHWREGS, GSPGPU_SERVHANDLEADR, HWREGS_ADDR, ROPKIT_TMPDATA+0x80, 4
-CALLFUNC_R0R1 svcSleepThread, 1000*1000000, 0
 
 @ Set the flag for terminating the GSP thread.
 @CALLFUNC_R0R1 svcSleepThread, 0x80000000,0
 ROPMACRO_WRITEWORD GSPTHREAD_OBJECTADDR+0x77, 0x1
 
-@ Lets kill these two threads before they crash the whole app!
+CALLFUNC_NOSP MEMSET32_OTHER+4, 0x0ffffb00, 0x400, 0x0fffff10, 0
+CALLFUNC_NOSP MEMSET32_OTHER+4, 0x0fffff00, 0xfc, svcSleepThread, 0
+CALLFUNC_NOSP MEMSET32_OTHER+4, 0x148d61a8-0x200, 0x210, 0x148d61a8, 0
+CALLFUNC_NOSP MEMSET32_OTHER+4, 0x148d61a8+0x10, 0x100, svcSleepThread, 0
+CALLFUNC_R0R1 svcSleepThread, 1000*1000000, 0
 
-@This is the main thread, just spam the current SP range with svcSleepThread. Sloppy and lazy but works OK, but sleepThread could expire in 5 minutes or 50 years based on unknown r1 value. Ill confirm this later, but time seems sufficiant for now.
-CALLFUNC_NOSP MEMSET32_OTHER+4, 0x0ffff800, 0x7f0, svcSleepThread, 0 
-@These memsets sprays are for the 2nd thread to kill. Its a sub-thread, but no less dangerous!
-CALLFUNC_NOSP MEMSET32_OTHER+4, 0x148d5b88-0x200, 0x210, 0x148d5b88, 0
-CALLFUNC_NOSP MEMSET32_OTHER+4, 0x148d5b88+0x10, 0x100, svcSleepThread, 0
-
-CALLFUNC_NOSP FS_MountExtSavedata, UBL_, 0, 0x308, 0
+CALLFUNC_NOSP FS_MountExtSavedata, UBL_, 0, 0x307, 0
 
 @ Load the file into the buffer.
 
 CALLFUNC_NOSP IFile_Open, ropkit_IFile_ctx, UBL_UBLL_LST, 1, 0
 
 
-CALLFUNC_NOSP IFile_Read, ropkit_IFile_ctx, ROPKIT_TMPDATA, ROPBUF, 0x5000
-CALLFUNC_R0R1 svcSleepThread, 100000000,0
+CALLFUNC_NOSP IFile_Read, ropkit_IFile_ctx, ROPKIT_TMPDATA, ROPKIT_LINEARMEM_BUF+0x20000, 0x5000
 
 .word POP_R0PC
-.word ROPBUF
+.word 0x14220000
 .word POP_R1PC
 .word ROP_POPPC
 .word STACK_PIVOT2
